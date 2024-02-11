05d54a33cfd7dc8e8684578adca0c8d84f4a8c23
arghyacodes <arghyaghoshal.juetce@gmail.com>
2024-02-11 13:25:36 +0530

    All Notes, Slides and Codes of DSA Supreme 2.0 are here
    DSA Supreme 2.0 documents

--- LeetCode/Topicwise/Array/rearrange_array_elements_by_sign.cpp
+++ LeetCode/Topicwise/Array/rearrange_array_elements_by_sign.cpp
@@ -1,1 +1,30 @@
 //leetcode #2149
+
+/*
+vector<int> rearrangeArray(vector<int>& nums) {
+       int n = nums.size();
+       vector<int> plus;
+       vector<int> minus;
+
+       for(int i = 0 ; i<n ; i++)
+       {
+           if(nums[i]>=0)
+           {
+               plus.push_back(nums[i]);
+           }
+           else
+           {
+               minus.push_back(nums[i]);
+           }
+       }
+        vector<int> ans;
+       for(int i = 0; i<n/2; i++)
+       {
+           ans.push_back(plus[i]);
+           ans.push_back(minus[i]);
+       }
+       plus.clear();
+       minus.clear();
+       return ans; 
+    }
+*/
--- LeetCode/Topicwise/Array/row_with_maximum_ones.cpp
+++ LeetCode/Topicwise/Array/row_with_maximum_ones.cpp
@@ -1,2 +1,25 @@
 // leetcode #2643
 
+/*
+vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
+        int maxOne = 0;
+        int row = 0;
+
+        for (int i = 0; i < mat.size(); i++) {
+            int oneCount = 0;
+            for (int j = 0; j < mat[i].size(); j++) {
+                if (mat[i][j] == 1) {
+                    oneCount++;
+                }
+            }
+            if (oneCount > maxOne) {
+                maxOne = oneCount;
+                row = i;
+            }
+        }
+        vector<int> ans;
+        ans.push_back(row);
+        ans.push_back(maxOne);
+        return ans;
+    }
+*/
